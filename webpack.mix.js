71c00ff54dfa2948db511b133249d607d582e56f
Emma Ramstedt <71063351+Deliciaes@users.noreply.github.com>
2022-03-15 20:09:48 +0100

    removed bootstrap js :boot:
    we don't use any js so no point in having it!

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/css/feed.scss', 'public/css',)
+mix.sass('resources/css/feed.scss', 'public/css',)
     .postCss('resources/css/app.css', 'public/css', [
     ]);
 
